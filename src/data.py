8e12bc1fec4a7406e3cd68a5dc7c36dd98d64cd6
innerNULL <lyt697@outlook.com>
2023-09-26 14:09:37 +0000

    feat: Text only model's dataset implementation

---
+++ src/data.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# file: data.py
+# date: 2023-09-23
+
+
+import pdb
+import traceback
+import json
+import duckdb
+import torch
+import pandas as pd
+from typing import List, Dict, Tuple
+from torch import LongTensor, FloatTensor
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
+
+from . import text
+
+
+class TextOnlyDataset(Dataset):
+    def __init__(self, 
+        data_path: str, data_dict_path: str, tokenizer: AutoTokenizer,
+        text_col: str="text", label_col: str="label", data_format: int="csv", 
+        chunk_size: int=512, chunk_num: int=2
+    ):
+        self.tokenizer: AutoTokenizer = tokenizer
+        self.data_path: str = data_path
+        self.data_dict: Dict[str, Dict] = json.loads(open(data_dict_path, "r").read())
+        self.text_col: str = text_col
+        self.label_col: str = label_col
+        self.chunk_size: int = chunk_size
+        self.chunk_num: int = chunk_num
+        self.data: List[Dict] = []
+
+        if data_format == "csv":
+            self.data = pd.read_csv(data_path)[[text_col, label_col]].to_dict(orient="records")
+        elif data_format == "jsonl":
+            self.data = duckdb.query(
+                "select %s from read_json_auto('%s');" % (text_col, data_path)
+            ).df()[[text_col, label_col]].to_dict(orient="records")
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, idx) -> Tuple[LongTensor, LongTensor, FloatTensor]:
+        record: Dict = self.data[idx]
+        txt: str = record[self.text_col]
+        label_names: List[str] = record[self.label_col].split(",")
+        label_ids: List[int] = [
+            self.data_dict["label2id"][x] for x in label_names 
+            if x in self.data_dict["label2id"]
+        ]
+        
+        label_one_hot: FloatTensor = torch.zeros(len(self.data_dict["label2id"]))
+        for label_id in label_ids:
+            label_one_hot[label_id] = 1.0
+
+        token_ids: List[List[int]] = []
+        token_masks: List[List[int]] = []
+
+        token_ids, token_masks = text.tokenize_to_chunks(
+            txt, self.tokenizer, self.chunk_size, self.chunk_num
+        )
+        
+        assert(label_one_hot.sum() >= 1)
+        return LongTensor(token_ids), LongTensor(token_masks), label_one_hot
